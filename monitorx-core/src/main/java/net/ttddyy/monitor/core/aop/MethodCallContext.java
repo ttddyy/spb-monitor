3886bcdfd6bbe1b0e81154eaa24ae99ce5376864
Tadaya Tsuyukubo <tadboy@t-pon.com>
2011-03-15 15:29:36 -0700

    add Serializable

--- monitorx-core/src/main/java/net/ttddyy/monitor/core/aop/MethodCallContext.java
+++ monitorx-core/src/main/java/net/ttddyy/monitor/core/aop/MethodCallContext.java
@@ -1,5 +1,6 @@
 package net.ttddyy.monitor.core.aop;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,7 @@ import java.util.List;
  */
 
 // TODO: should this to be Serializable
-public class MethodCallContext {
+public class MethodCallContext implements Serializable {
 
     private List<MethodCallEntry> entries = new ArrayList<MethodCallEntry>();
 
