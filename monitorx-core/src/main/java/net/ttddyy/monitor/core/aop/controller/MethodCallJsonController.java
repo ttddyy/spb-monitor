24b6bb434fa7bd1e7c718a5e2f92652a5c6788b5
Tadaya Tsuyukubo <tadboy@t-pon.com>
2011-03-28 17:53:12 -0700

    initial controller impl

---
+++ monitorx-core/src/main/java/net/ttddyy/monitor/core/aop/controller/MethodCallJsonController.java
@@ -0,0 +1,35 @@
+package net.ttddyy.monitor.core.aop.controller;
+
+import net.ttddyy.monitor.core.aop.MethodCallContext;
+import net.ttddyy.monitor.core.aop.MethodCallContextRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * @author Tadaya Tsuyukubo
+ */
+public class MethodCallJsonController {
+
+    private MethodCallContextRepository repository;
+
+
+    @RequestMapping(value = "/json/all")
+    public ModelAndView getAll() {
+
+        final List<MethodCallContext> contexts = repository.getAll();
+
+        final ModelAndView mav = new ModelAndView(new MappingJacksonJsonView());
+        mav.addObject("contexts", contexts);
+        return mav;
+    }
+
+    @Autowired
+    public void setRepository(MethodCallContextRepository repository) {
+        this.repository = repository;
+    }
+}
