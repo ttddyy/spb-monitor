5138e9fbddccfb8a629d8844c31aadc54293bcf6
Tadaya Tsuyukubo <tadboy@t-pon.com>
2011-03-28 20:36:00 -0700

    resizable in-memory repository

--- monitorx-core/src/main/java/net/ttddyy/monitor/core/aop/InMemoryMethodCallContextRepository.java
+++ monitorx-core/src/main/java/net/ttddyy/monitor/core/aop/InMemoryMethodCallContextRepository.java
@@ -6,17 +6,34 @@ import java.util.List;
 /**
  * @author Tadaya Tsuyukubo
  */
-public class MethodCallContextRepositoryListImpl implements MethodCallContextRepository {
+public class InMemoryMethodCallContextRepository implements MethodCallContextRepository {
 
     private List<MethodCallContext> list = new ArrayList<MethodCallContext>();
 
+    private int maxSize = -1;
 
     public void persist(MethodCallContext context) {
+        resize();
         list.add(context);
     }
 
     public List<MethodCallContext> getAll() {
         // TODO: should it be Collections.unmodifiableList() ??
         return new ArrayList<MethodCallContext>(list);
+    }
+
+    private void resize() {
+        if (maxSize < 0) {
+            return;
+        }
+
+        while (list.size() > maxSize) {
+            list.remove(0);
+        }
+
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
     }
 }
